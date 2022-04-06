1a70cf6d7e1fb165a562322de2ca638679fc0f1a
victormicco <victor.oficial093@gmail.com>
2022-04-05 22:51:17 -0300

    Subdivision Component

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import Subdivisao from "../components/Subdivisao";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Subdivisao preta />
+      <Subdivisao />
+      <Subdivisao preta />
+    </div>
+  );
+}
